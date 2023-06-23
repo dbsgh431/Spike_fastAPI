e41f1a17507dbb75d479f24f29cbd7b8f584b11f
yoonho <qufdbsgh431@gmail.com>
2023-06-23 20:45:24 +0900

    SQLModel 모듈을 활용한 sqlite 설정 방법

--- planner/models/event.py
+++ planner/models/event.py
@@ -1,13 +1,33 @@
+from sqlmodel import JSON, SQLModel, Field, Column
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 
-class Event(BaseModel):
-    id : int
+class Event(SQLModel, table=True):
+    id : int = Field(default=None, primary_key=True)
     title : str
     image : str
     description : str
-    tags : List[str]
+    tags : List[str] = Field(sa_column=Column(JSON))
     location : str
+
+    class Config:
+        arbitrary_types_allowed = True
+        schema_extra = {
+            "example" : {
+                "title" : "FastAPI WEB Develop",
+                "image" : "https://linktomyimage.com/image.png",
+                "description" : "MY planner",
+                "tags" : ["study", "sports", "movie"],
+                "location" : "home"
+            }
+        }
+
+class EventUpdate(SQLModel):
+    title : Optional[str]
+    image : Optional[str]
+    description : Optional[str]
+    tags : Optional[List[str]]
+    location : Optional[str]
 
     class Config:
         schema_extra = {
